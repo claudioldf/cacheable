f4852cb145bb47b06507b2b1171a418ef78d5f45
Claudio Dias <claudioldf@gmail.com>
2017-11-20 11:24:50 -0200

    Update GiverXCacheStore.php
    Adicionado métodos de suporte ao Illuminate 5.3 - 5.4

--- src/GiverXCacheStore.php
+++ src/GiverXCacheStore.php
@@ -203,4 +203,34 @@ class GiverXCacheStore extends \Illuminate\Cache\TaggableStore implements \Illum
         return xcache_unset($id);
     }
 
+    /**
+     * Retrieve multiple items from the cache by key.
+     *
+     * Items not found in the cache will have a null value.
+     *
+     * @param  array  $keys
+     * @return array
+     */
+    public function many(array $keys) {
+        $data = [];
+
+        foreach($keys as $key) {
+            $data[$this->prefix.$key] = $this->get($key);
+        }
+
+        return $data;
+    }
+
+    /**
+     * Store multiple items in the cache for a given number of minutes.
+     *
+     * @param  array  $values
+     * @param  int  $minutes
+     * @return void
+     */
+    public function putMany(array $values, $minutes) {
+        foreach($values as $key => $value) {
+            $this->put($key, $value, $minutes);
+        }
+    }
 }
